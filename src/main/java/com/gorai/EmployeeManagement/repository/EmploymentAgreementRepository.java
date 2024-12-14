ddb3e0a99edecd1a5e240cffad827d150bbd56cd
DivyaMS27 <143314353+DivyaMS27@users.noreply.github.com>
2024-12-14 22:19:48 +0530

    Update EmploymentAgreementRepository.java

--- src/main/java/com/gorai/EmployeeManagement/repository/EmploymentAgreementRepository.java
+++ src/main/java/com/gorai/EmployeeManagement/repository/EmploymentAgreementRepository.java
@@ -4,4 +4,5 @@ import com.gorai.EmployeeManagement.entity.EmploymentAgreement;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmploymentAgreementRepository extends JpaRepository<EmploymentAgreement, Long> {
+  List<EmploymentAgreement> findByEmployeeNameContainingOrRoleContaining(String employeeName,String role);
 }
